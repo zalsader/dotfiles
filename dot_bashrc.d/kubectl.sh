7e301187945746917901722506af4baa32e236db
Zuhair AlSader <zuhair@koor.tech>
2024-03-15 21:54:56 -0400

    add multi kubeconfig

--- dot_bashrc.d/kubectl.sh
+++ dot_bashrc.d/kubectl.sh
@@ -2,3 +2,10 @@ alias watch='watch '
 source ~/.bash_completion.d/complete_alias
 alias k=kubectl
 complete -F _complete_alias k
+
+for kconfig in $HOME/.kube/config $(find $HOME/.kube/ -iname "*.config")
+do
+  if [ -f "$kconfig" ];then
+     export KUBECONFIG=$KUBECONFIG:$kconfig
+   fi
+ done
