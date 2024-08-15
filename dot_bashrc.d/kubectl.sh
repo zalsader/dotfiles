93e0163f8cd0aadc70b610f6b46653b55061fe6f
Zuhair AlSader <zuhair@devzero.io>
2024-08-14 22:52:53 -0400

    add kx and kns

--- dot_bashrc.d/kubectl.sh
+++ dot_bashrc.d/kubectl.sh
@@ -3,6 +3,12 @@ source ~/.bash_completion.d/complete_alias
 alias k=kubectl
 complete -F _complete_alias k
 
+alias kx='kubectl ctx'
+complete -F _complete_alias kx
+
+alias kns='kubectl ns'
+complete -F _complete_alias kns
+
 for kconfig in $HOME/.kube/config $(find $HOME/.kube/ -iname "*.config")
 do
   if [ -f "$kconfig" ];then
